b6986f8bfb82e10a5a8b88681358ab2a6b8b091e
AsadUnityDev <asad@narsun.pk>
2022-05-17 14:21:46 +0500

    Some Apk Issues

---
+++ Assets/Scripts/TransparencyEyesMouth.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransparencyEyesMouth : MonoBehaviour
+{
+    public Material transparent;
+    public Material[] AllMaterials;
+    public Material[] TransparentMaterials;
+    public PlayerController playerRef;
+    bool materialsChanged;
+    bool materialsReset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        AllMaterials = gameObject.GetComponent<SkinnedMeshRenderer>().materials;
+        TransparentMaterials = new Material[gameObject.GetComponent<SkinnedMeshRenderer>().materials.Length];
+        for (int i = 0; i < gameObject.GetComponent<SkinnedMeshRenderer>().materials.Length; i++)
+        {
+
+            TransparentMaterials[i] = transparent;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerRef.InvisibilityBool && !materialsChanged)
+        {
+            gameObject.GetComponent<SkinnedMeshRenderer>().materials = TransparentMaterials;
+        }
+        else
+        {
+            gameObject.GetComponent<SkinnedMeshRenderer>().materials = AllMaterials;
+        }
+    }
+}
