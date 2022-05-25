4026cfa32a84f637a088e34d6476c5480dc5670f
Barira631 <77618725+Barira631@users.noreply.github.com>
2022-05-25 18:36:54 +0500

    CS-229  #Completed

--- Assets/Scripts/APIManager.cs
+++ Assets/Scripts/APIManager.cs
@@ -115,7 +115,7 @@ public class APIManager : MonoBehaviour
         //Invoke("SetCharacterApi", 2f);
         //SetCharacterApi();
         //GetCharacterAPI();
-        //////////
+      
     }
 
     public void GetProfileAPI()
