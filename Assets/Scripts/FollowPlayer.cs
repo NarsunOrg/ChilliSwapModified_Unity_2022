c77885ede5dd1054664435e07c13bbd0c6dbf002
Barira631 <barira@narsun.pk>
2022-06-17 01:10:50 +0500

    jerk issue fixed, Animation fixed

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -9,16 +9,41 @@ public class FollowPlayer : MonoBehaviour
     public GameObject PositionToSet;
     public Vector3 offset;
     public static float lookatspeed=1f;
+    float sSpeed = 1f;
     
-    void Start()
+    //void Update()
+    //{
+    //    this.transform.position = PositionToSet.transform.position;
+    //   // transform.LookAt(Player.transform);
+    //    //this.transform.rotation = PositionToSet.transform.rotation;
+    //    // gameObject.transform.DOLocalMove(PositionToSet.transform.position, 0.2f);
+    //    //gameObject.transform.DOLookAt(Player.transform.position, lookatspeed);
+    //}
+    // camera will follow this object
+    public Transform Target;
+    //camera transform
+   // public Transform camTransform;
+    // offset between camera and target
+    public Vector3 Offset;
+    // change this value to get desired smoothness
+    public float SmoothTime = 0.01f;
+
+    // This value will change at the runtime depending on target movement. Initialize with zero vector.
+    private Vector3 velocity = Vector3.zero;
+
+    private void Start()
     {
-        
+      //  Offset = this.transform.position - Target.position;
     }
 
-   
-    void Update()
+    private void LateUpdate()
     {
-        gameObject.transform.DOLocalMove(PositionToSet.transform.position, 0.2f);
-        gameObject.transform.DOLookAt(Player.transform.position, lookatspeed);
+        // update position
+        //Vector3 targetPosition = Target.position + Offset;
+        this.transform.position = Vector3.SmoothDamp(transform.position, PositionToSet.transform.position, ref velocity, SmoothTime);
+
+        // update rotation
+        transform.LookAt(Target);
     }
+
 }
